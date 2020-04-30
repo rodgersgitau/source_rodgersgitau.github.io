3a473beaa328f4a03fa0f5342aa650a0ee0f8fbc
Valdadyne <rodgers.gitau@gmail.com>
2020-04-30 12:58:50 +0300

    feat: public .git submodule

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,7 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Empty public folder
-rm -rf public/*
- 
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
@@ -25,4 +23,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master -f
+git push origin master
